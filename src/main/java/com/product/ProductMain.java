db3a5eb8160ecd3763457c8a8f1f16eb4a89a29d
KajalMathur <KajalMathur@users.noreply.github.com>
2019-12-22 11:16:29 +0530

    Ribbon (#20)
    With Zuul and Ribbon configuration.

--- src/main/java/com/product/ProductMain.java
+++ src/main/java/com/product/ProductMain.java
@@ -2,11 +2,13 @@ package com.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 import lombok.extern.slf4j.Slf4j;
 
+@EnableEurekaClient
 @SpringBootApplication
 @Slf4j
 public class ProductMain {
